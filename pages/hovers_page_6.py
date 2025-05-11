952c125b196f7783b54f76d949e515a2a45f67ed
ashenwarrior <myemail@whatever.com>
2025-05-11 19:53:44 +0300

    First fix, tests working

--- pages/hovers_page_6.py
+++ pages/hovers_page_6.py
@@ -1,3 +1,4 @@
+from elements.label import Label
 from .base_page import BasePage
 from elements.web_element import WebElement
 from selenium.webdriver import ActionChains
@@ -5,39 +6,34 @@ from selenium.webdriver import ActionChains
 
 class HoversPage(BasePage):
     UNIQUE_ELEMENT = "//*[contains(text(), 'Hovers')]"
-    USER_1_AVATAR = "(//div[@class='figure']/img)[1]"
-    USER_2_AVATAR = "(//div[@class='figure']/img)[2]"
-    USER_3_AVATAR = "(//div[@class='figure']/img)[3]"
-    VIEW_PROFILE = "//a[contains(text(), 'View profile')]"
     USER_NAME = "//*[contains(text(), 'name:')]"
 
     def __init__(self, browser):
         super().__init__(browser)
-        self.browser = browser
-        self.driver = browser.driver
-        self.unique_element = WebElement(self.browser, self.UNIQUE_ELEMENT)
-        self.user_1_avatar = WebElement(self.browser, self.USER_1_AVATAR)
-        self.user_2_avatar = WebElement(self.browser, self.USER_2_AVATAR)
-        self.user_3_avatar = WebElement(self.browser, self.USER_3_AVATAR)
-        self.view_profile = WebElement(self.browser, self.VIEW_PROFILE)
-        self.user_name = WebElement(self.browser, self.USER_NAME)
+        self.unique_element = Label(self.browser, self.UNIQUE_ELEMENT)
 
-    def hover_avatar(self, user: int) -> tuple[str, str]:
-        if user == 1:
-            self.user_1_avatar.wait_for_visible()
-            ActionChains(self.driver).move_to_element(self.user_1_avatar.native_element).perform()
-        elif user == 2:
-            self.user_2_avatar.wait_for_visible()
-            ActionChains(self.driver).move_to_element(self.user_2_avatar.native_element).perform()
-        elif user == 3:
-            self.user_3_avatar.wait_for_visible()
-            ActionChains(self.driver).move_to_element(self.user_3_avatar.native_element).perform()
+    def hover_avatar(self, user_id: int) -> None:
+        user_avatar_locator = f"//*[contains(text(), 'name: user{user_id}')]/../../img"
+        if user_id in range(1,4):
+            user_avatar = WebElement(self.browser, user_avatar_locator)
+            ActionChains(self.driver).move_to_element(user_avatar.wait_for_visible()).perform()
         else:
             raise ValueError("Неверный user, доступны значения от 1 до 3")
-        self.view_profile.wait_for_presence()
-        self.user_name.wait_for_visible()
-        return self.user_name.get_text(), self.view_profile.get_attribute('href')
 
-    def go_to_user_profile(self):
-        self.view_profile.wait_for_clickable()
-        self.view_profile.click()
+    def get_username(self, user_id: int) -> str:
+        user_name_locator = f"//*[contains(text(), 'name: user{user_id}')]"
+        user_name = Label(self.browser, user_name_locator)
+        user_name.wait_for_visible()
+        return user_name.get_text()
+
+    def get_profile_link(self, user_id: int) -> str:
+        view_profile_locator = f"//*[contains(text(), 'name: user{user_id}')]/../a"
+        view_profile = WebElement(self.browser, view_profile_locator)
+        view_profile.wait_for_visible()
+        return view_profile.get_attribute("href")
+
+    def go_to_user_profile(self, user_id: int) -> None:
+        view_profile_locator = f"//*[contains(text(), 'name: user{user_id}')]/../a"
+        view_profile = WebElement(self.browser, view_profile_locator)
+        view_profile.wait_for_clickable()
+        view_profile.click()
