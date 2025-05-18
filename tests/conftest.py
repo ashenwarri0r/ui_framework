d4a5468bc056c873a6220fd48057d8d7c40b7a9d
ashenwarrior <myemail@whatever.com>
2025-05-19 00:59:44 +0300

    added options

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium.webdriver import Chrome
 from browser.browser import Browser
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument("--headless=new")
 
 @pytest.fixture(scope="function")
 def browser():
-    driver = Chrome()
+    driver = Chrome(options=options)
     browser = Browser(driver)
     yield browser
     browser.quit()
