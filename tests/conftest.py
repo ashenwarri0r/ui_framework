d0fb1fdb9e53600e2b4ac6af6930500c3492be81
ashenwarrior <myemail@whatever.com>
2025-05-19 18:47:11 +0300

    options for docker

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,10 @@ from browser.browser import Browser
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-# options.add_argument("--headless=new")
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
 
 @pytest.fixture(scope="function")
 def browser():
